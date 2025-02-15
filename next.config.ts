2f0a0944e1c7c3d4aadc87acac4661e4d2e85320
stelee410 <129747297+stelee410@users.noreply.github.com>
2025-02-15 13:44:36 +0800

    add server proxy so that this UI can work with the jess main project

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ const withNextIntl = require('next-intl/plugin')();
  
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */ 
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: 'http://127.0.0.1:8080/api/v1/:path*',
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
